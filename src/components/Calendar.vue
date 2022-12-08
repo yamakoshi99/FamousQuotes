80b4981eca9097d4beaf5760bb544b861967993e
yamakoshi <takuyageemzuki@gmai.com>
2022-12-08 23:53:39 +0900

    カレンダーコンポーネント追加

---
+++ src/components/Calendar.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-date-picker
+    v-model="date"
+    type="date"
+    placeholder="カレンダーを選択">
+  </el-date-picker>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: ''
+    }
+  }
+}
+</script>
